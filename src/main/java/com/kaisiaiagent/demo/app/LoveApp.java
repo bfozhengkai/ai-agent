dce6a2c233c3a626564ba7e4e3750f21eeecc45e
bfozhengkai <kaisizheng@hotmail.com>
2025-06-01 18:25:52 -0400

    ai-agent-1: Add customized advisor.

--- src/main/java/com/kaisiaiagent/demo/app/LoveApp.java
+++ src/main/java/com/kaisiaiagent/demo/app/LoveApp.java
@@ -1,8 +1,10 @@
 package com.kaisiaiagent.demo.app;
 
+import com.kaisiaiagent.demo.advisor.MyLoggerAdvisor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
@@ -29,7 +31,8 @@ public class LoveApp {
         chatClient  = ChatClient.builder(dashscopeChatModel)
                 .defaultSystem(SYSTEM_PROMPT)
                 .defaultAdvisors(
-                        new MessageChatMemoryAdvisor(chatMemory)
+                        new MessageChatMemoryAdvisor(chatMemory),
+                        new MyLoggerAdvisor()
                 )
                 .build();
     }
